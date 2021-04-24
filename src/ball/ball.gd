9dd3b913135e9cf8de8795acd38840ea5ac8b935
Jose T. Monagas <jmonagas@localhost.localdomain>
2021-04-24 14:19:32 -0400

    Create ball scene

---
+++ src/ball/ball.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+export var acceleration: int = 100
+
+var velocity: Vector2 = Vector2()
+
+
+func _ready() -> void:
+	velocity = Vector2(-1, 1) * acceleration
+
+
+func _physics_process(delta: float) -> void:
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	
+	if collision:
+		velocity = velocity.bounce(collision.normal)
+		
+		if collision.collider.has_method("hit"):
+			collision.collider.hit()
